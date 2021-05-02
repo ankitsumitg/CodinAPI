fd848a21bc57b99a27791851619e7748a08069a1
Ankit Gupta <ankitsumitg@gmail.com>
2021-05-03 03:11:31 +0530

    semi final code

--- py/example/login_and_launch.py
+++ py/example/login_and_launch.py
@@ -1,9 +1,12 @@
 import asyncio
-
 from src.main import CodinAPI
+
+
 async def main():
     user = CodinAPI()
-    json = await user.login('canrrwxnectmuqptat@upived.online','12345678')
-    print(json)
+    await user.login(input('Enter Email: '), input('Enter Password'))
+    await user.create_clash()
+    print(f'Url to clash is : {user.private_clash_url}')
+
 
 asyncio.run(main())
--- py/src/main.py
+++ py/src/main.py
@@ -1,22 +1,30 @@
 import asyncio
 import aiohttp
+import requests
+from json import dumps
 from .endpoints import Endpoints
 
 
 class CodinAPI:
     def __init__(self):
         self.loop = asyncio.get_event_loop()
-        self._session = aiohttp.ClientSession(loop=self.loop)
+        self._session = requests.Session()
         self.logged_in = False
         self.codin_user = None
+        self.user_id = None
+        self.private_clash_url = 'https://www.codingame.com/clashofcode/clash/'
 
+    async def login(self, email, password):
+        headers = {'Content-Type': 'application/json', 'Accept': '*/*'}
+        data = [email, password, True]
+        r = self._session.post(Endpoints.login, json=data, headers=headers)
+        self.codin_user = r.json()
+        self.user_id = self.codin_user['userId']
 
-    async def login(self, email: str, password: str):
-        r = self._session.post(Endpoints.login, json=[email, password, True])
-        json = await r.json()
-        self.codin_user = json
-        self.logged_in = True
-        return json
-
+    async def create_clash(self):
+        headers = {'Content-Type': 'application/json', 'Accept': '*/*'}
+        data = [self.user_id, {'SHORT': True}, [], ["FASTEST", "SHORTEST", "REVERSE"]]
+        r = self._session.post(Endpoints.createClash, json=data, headers=headers)
+        self.private_clash_url += r.json()['publicHandle']
     async def close(self):
         await self._session.close()
