b80c583da55f8c9c3329b32ebb3950a713d892f9
Ankit Gupta <ankitsumitg@gmail.com>
2021-05-02 21:34:23 +0530

    add more files

---
+++ py/src/endpoints.py
@@ -0,0 +1,7 @@
+class Endpoints:
+    login = "https://www.codingame.com/services/Codingamer/loginSiteV2"
+    getUserWithHandle = "https://www.codingame.com/services/CodinGamer/findCodingamePointsStatsByHandle"
+    createClash = "https://www.codingame.com/services/ClashOfCode/createPrivateClash"
+    startClash = "https://www.codingame.com/services/ClashOfCode/startClashByHandle"
+    getClash = "https://www.codingame.com/services/ClashOfCode/findClashByHandle"
+    getPendingClashes = "https://www.codingame.com/services/ClashOfCode/findPendingClashes"
