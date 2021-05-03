57a266fe04c65ad00613f24a5314aff1373b1542
Ankit Gupta <ankitsumitg@gmail.com>
2021-05-03 20:47:19 +0530

    Added exception handling with some extra modification.

--- py/example/login_and_launch.py
+++ py/example/login_and_launch.py
@@ -6,7 +6,7 @@ async def main():
     user = CodinAPI()
     await user.login(input('Enter Email: '), input('Enter Password: '))
     await user.create_clash()
-    print(f'Url to clash is : {user.private_clash_url}')
+    print(f'Url to clash is : {user.get_clash_url()}')
     await user.close()
 
 
--- py/src/main.py
+++ py/src/main.py
@@ -1,5 +1,7 @@
 import asyncio
+
 import requests
+
 from .endpoints import Endpoints
 
 
@@ -13,17 +15,31 @@ class CodinAPI:
         self.private_clash_url = None
 
     async def login(self, email, password):
-        headers = {'Content-Type': 'application/json', 'Accept': '*/*'}
-        data = [email, password, True]
-        r = self._session.post(Endpoints.login, json=data, headers=headers)
-        self.codin_user = r.json()
-        self.user_id = self.codin_user['userId']
+        try:
+            headers = {'Content-Type': 'application/json', 'Accept': '*/*'}
+            data = [email, password, True]
+            r = self._session.post(Endpoints.login, json=data, headers=headers)
+            self.codin_user = r.json()
+            if 'userId' not in self.codin_user:
+                raise Exception(f'{"*" * 10} {self.codin_user["message"]} {"*" * 10}')
+            self.user_id = self.codin_user['userId']
+            self.logged_in = True
+        except Exception as e:
+            print(e)
 
     async def create_clash(self):
+        if not self.logged_in:
+            print(f'{"*" * 10} Not logged in {"*" * 10}')
+            return
         headers = {'Content-Type': 'application/json', 'Accept': '*/*'}
         data = [self.user_id, {'SHORT': True}, [], ["FASTEST", "SHORTEST", "REVERSE"]]
         r = self._session.post(Endpoints.createClash, json=data, headers=headers)
         self.private_clash_url = 'https://www.codingame.com/clashofcode/clash/' + r.json()['publicHandle']
+
+    def get_clash_url(self):
+        if not self.private_clash_url:
+            return f'{"*" * 10} Clash not created {"*" * 10}'
+        return self.private_clash_url
 
     async def close(self):
         self._session.close()
